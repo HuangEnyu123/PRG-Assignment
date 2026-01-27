0591b7f77e5d82d8c84b757151287f7aa8d3b8b9
HuangEnyu1 <s10274277E@connect.np.edu.sg>
2026-01-27 15:47:03 +0800

    Second Commit

--- obj/Release/net8.0/S10274277E_Assignment.AssemblyInfo.cs
+++ obj/Release/net8.0/S10274277E_Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S10274277E_Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e145ffdb947147ff50cedca7ed2c1f7c20ab525a")]
 [assembly: System.Reflection.AssemblyProductAttribute("S10274277E_Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S10274277E_Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
