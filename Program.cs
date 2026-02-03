66e2c37b6089be7c0bb18ee979164311e3720447
HuangEnyu1 <s10274277E@connect.np.edu.sg>
2026-02-03 15:18:58 +0800

    Changed Basic Feature 2

--- Program.cs
+++ Program.cs
@@ -2,14 +2,14 @@
  Student Number: S10274277E
  Student Name: Huang Enyu 
  Partner Name: (Solo)
-*/ 
+*/
 
 /*Basic Feature 1*/
 
+/*First Basic Feature Commit*/
 
-
-
-
+//
+Console.WriteLine("Testing Basic Feature");
 
 
 
